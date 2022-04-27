7aaef7c7bcbbeec7bfccdd8274b0286afa34be48
devbrown23 <devbrownrealtor@gmail.com>
2022-04-27 09:32:25 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function removeElement(element){
+    element.remove()
+}
+
+function logout(element) {
+    element.innerText = "logout";
+}
+
+function alert(element) {
+    element.alert = "Ninja was liked";
+}
